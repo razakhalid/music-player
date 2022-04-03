05a6c1c9ad47a82b80d334d0a06949ffb44b7e00
Raza Khalid <ra97za@gmail.com>
2022-04-03 02:09:03 -0500

    setup dynamic progress bar

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvEhYz9Bn52k04iJo7w_CZn7iWM2jwUZM",
   authDomain: "music-player-366ae.firebaseapp.com",
   projectId: "music-player-366ae",
   storageBucket: "music-player-366ae.appspot.com",
+  // storageBucket: "gs://music-player-366ae.appspot.com",
   appId: "1:912146749476:web:6d6402b73936799583a0b1"
 };
 
@@ -15,11 +17,13 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
 export {
   auth,
   db,
-  usersCollection
+  usersCollection,
+  storage
 }
