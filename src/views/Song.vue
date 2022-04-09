2812c777cd164edaaa7fe431b6966f25940e975a
Raza Khalid <ra97za@gmail.com>
2022-04-09 17:10:24 -0500

    query params for sorting

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -108,6 +108,10 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -147,6 +151,16 @@ export default {
           ...document.data()
         })
       })
+    }
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+      this.$router.push({
+        query: { sort: newVal }
+      });
     }
   }
 }
